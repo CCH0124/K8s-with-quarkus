4a7b7e6def9790ce78247ccd5e59b2888e965499
CCH0124 <cchong0124@gmail.com>
2023-12-24 12:18:39 +0800

    init

---
+++ helm-kz-k8s/src/main/java/org/cch/router/KubernetesRouterBuilder.java
@@ -0,0 +1,12 @@
+package org.cch.router;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class KubernetesRouterBuilder extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        
+    }
+    
+}
